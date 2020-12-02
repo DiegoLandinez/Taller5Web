626edda402c863dcb6bd1c3551822343620d30eb
mannuluss <49813014+mannuluss@users.noreply.github.com>
2020-12-02 14:54:43 -0500

    create function CedulaUser

--- behaviour.js
+++ behaviour.js
@@ -20,3 +20,15 @@ function validateDireccion(id){
     if (textfield.value.indexOf("anv")){}
     if (textfield.value.indexOf("trans")){}
 }
+function cedulaUser(id){
+    var element = document.getElementById(id);
+    var charRare = isNaN(parseInt(element.value,2));//caracteres extraños, son los que no se pueden castear a numeros
+
+    if (element.value > 20 || charRare){//cuando tiene una longitud mayor de 20 char
+        element.style.borderColor = rgb(255,0,0);//rojo
+    }else{
+        if (element.value < 20 && element.value > 10){
+            element.style.borderColor = rgb(0,255,0);//verde
+        }
+    }
+}
